4f5b99f9bcd1a4fc47e40ae0cd5dfc83361fb10e
beevekmgr <beevekmgr@gmail.com>
2021-09-02 19:21:19 +0545

    feat: Added chat screen

--- lib/screens/chats/components/body.dart
+++ lib/screens/chats/components/body.dart
@@ -2,6 +2,7 @@ import 'package:chat/components/filled_outline_button.dart';
 import 'package:chat/constants.dart';
 import 'package:chat/models/Chat.dart';
 import 'package:chat/screens/chats/components/chat_card.dart';
+import 'package:chat/screens/messages/message_screen.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
@@ -33,7 +34,11 @@ class Body extends StatelessWidget {
           itemCount: chatsData.length,
           itemBuilder: (context, index) => ChatCard(
             chat: chatsData[index],
-            press: () {},
+            press: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MessagesScreen(),
+                )),
           ),
         ))
       ],
